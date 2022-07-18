fccce1e9179384f922acf5272be18287d258d58f
Fran1808 <francmaci01@gmail.com>
2022-07-18 11:09:19 -0500

    primer proyecto

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    import { createRouter, createWebHistory } from 'vue-router'
+    import NavBarComponent from "../components/NavBarComponent.vue";
+    import TableComponent from "../components/TableComponent.vue";
+</script>
+
+<template>
+    <NavBarComponent />
+    <TableComponent :title="name"/>
+
+</template>
+
+<style>
+
+</style>
+
+<script lang="ts">
+export default {
+        data() {
+        return {
+            name: "Veronica"
+        }
+    }
+}
+</script>
+<!---->
